ec467323962fa1e0d41361dc772ca48dced1fe9c
Marcos Araujo <marcos_4raujo@outlook.com>
2023-11-19 02:22:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = "CLAVE ULTRA SECRETA";
 const path = require('path');
+const mariadb = require("mariadb");
+
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "user_information",
+    connectionLimit: 5,
+});
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-})
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
